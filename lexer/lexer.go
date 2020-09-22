e536dbbad7455304e722fe7e1eac0beef8ad3195
Mark <the.mark.fullton@gmail.com>
2020-09-22 13:09:22 -0700

    Added NextToken()

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go-brainer-interpreter/token"
+
 type Lexer struct {
 	input        string
 	position     int  //keeps track of current char
@@ -7,20 +9,57 @@ type Lexer struct {
 	ch           byte // the char being examined
 }
 
+// This will only support ASCI because ch set to byte
+// To use UTF-8, ch would need to be rune and we would need to change the read function
+
+// Go back and figure out how to change to UTF-8 after finishing book
+
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+
+	l.readChar()
 
 	return l
 }
 
 // readChar reads character and advances current position
 func (l *Lexer) readChar() {
+	// Checks if we're at the end of the line, if so, sets to 0/NUL
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 
+	// Ensure l.readPosition always points to the next position that will be read and l.position always points to position where we last read.
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch{
+	case '=':
+		tok = newToken(token.ASSIGN, 1.ch)
+case ';':
+	tok = newToken(token.SEMICOLON, 1.ch)
+case '(':
+	tok = newToken(token.LPAREN, l.ch)
+case ')':
+	tok = newToken(token.RPAREN, l.ch)
+case ',':
+	tok = newToken(token.COMMA, l.ch)
+case '+':
+	tok = newToken(token.PLUS, l.ch)
+case '{':
+	tok = newToken(token.LBRACE, l.ch)
+case '}':
+	tok = newToken(token.RBRACE, l.ch)
+case 0:
+	tok.Literal = ""
+
+	tok.Type = token.EOF
+
+}
 }
