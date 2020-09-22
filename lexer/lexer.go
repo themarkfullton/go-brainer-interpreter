8df317f27360ac0dbf41033fa7e7e4308c51ed8e
Mark <the.mark.fullton@gmail.com>
2020-09-22 10:53:07 -0700

    Added readChar function in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -12,3 +12,15 @@ func New(input string) *Lexer {
 
 	return l
 }
+
+// readChar reads character and advances current position
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
