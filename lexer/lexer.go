10445eaa0439b6ca885490306bd28d4a5ffb6639
Mark <the.mark.fullton@gmail.com>
2020-09-26 23:25:22 -0700

    Added isLetter to Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -59,6 +59,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch){
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -68,4 +75,20 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token { Type: tokenType, Literal: string(ch)}
+}
+
+// Reads an identifier and advances lexer's position until finds non-letter char
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	for isLetter(l.ch){
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+// Allows _ to be used in variable names
+func isLetter(ch byte) bool{
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
