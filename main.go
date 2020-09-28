d768d40bb28d28ca244ea36b1e8cd7e46db52d1f
Mark <the.mark.fullton@gmail.com>
2020-09-27 22:38:17 -0700

    Added main.go

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
+	"github.com/themarkfullton/go-brainer-interpreter/repl"
 )
 
 func main() {
-	fmt.Println("Placeholder")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! I am the Go-Brainer interpreter!\n\n", user.Username)
+
+	fmt.Printf("Please enter some code for me to interpret!\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
