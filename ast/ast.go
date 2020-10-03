dc0fc227b7152d3067533a2f0c408af1566ea369
Mark <the.mark.fullton@gmail.com>
2020-10-02 23:37:49 -0700

    Added Let and Identifier to ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/themarkfullton/go-brainer-interpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -20,10 +22,33 @@ type Program struct {
 	Statements []Statement
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
 func (p *Program) TokenLiteral() string{
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+func (ls *LetStatement) statmentNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string{
+	return i.TokenLiteral
 }
