2b69786c33c1614ca286b248617419c462a2fb6d
Mark <the.mark.fullton@gmail.com>
2020-12-15 12:12:56 -0800

    Overwrote String statements for various token expression nodes in AST

--- ast/ast.go
+++ ast/ast.go
@@ -10,16 +10,6 @@ type Node interface {
 	String() string
 }
 
-func (p *Program) String() string {
-	var out bytes.Buffer
-
-	for _, s := range p.Statements {
-		out.WriteString(s.String())
-	}
-
-	return out.String()
-}
-
 type Statement interface {
 	Node
 
@@ -36,25 +26,14 @@ type Program struct {
 	Statements []Statement
 }
 
-type LetStatement struct {
-	Token token.Token
-	Name *Identifier
-	Value Expression
-}
+func (p *Program) String() string {
+	var out bytes.Buffer
 
-type Identifier struct {
-	Token token.Token
-	Value string
-}
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
 
-type ReturnStatement struct {
-	Token token.Token
-	ReturnValue Expression
-}
-
-type ExpressionStatement struct {
-	Token token.Token
-	Expression Expression
+	return out.String()
 }
 
 func (p *Program) TokenLiteral() string{
@@ -65,10 +44,38 @@ func (p *Program) TokenLiteral() string{
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+
+type Identifier struct {
+	Token token.Token
+	Value string
 }
 
 func (i *Identifier) expressionNode() {}
@@ -77,10 +84,42 @@ func (i *Identifier) TokenLiteral() string{
 	return i.Token.Literal
 }
 
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
 }
 
 func (es *ExpressionStatement) statementNode() {
