d76e42bdc4d634bcf5667aac50623c44d44430ee
Mark <the.mark.fullton@gmail.com>
2021-01-09 18:29:54 -0800

    Parser - Added test for prefix

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"testing"
+	"fmt"
 	"github.com/themarkfullton/go-brainer-interpreter/ast"
 	"github.com/themarkfullton/go-brainer-interpreter/lexer"
 )
@@ -203,4 +204,28 @@ func TestParsingPrefixExpressions(t *testing.T) {
 			return
 		}
 	}
+}
+
+func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool{
+	integ, ok := il.(*ast.IntegerLiteral)
+
+	if !ok {
+		t.Errorf("il not *ast.IntegerLiteral. got=%T", il)
+
+		return false
+	}
+
+	if integ.Value != value {
+		t.Errorf("integ.Value not %d. got=%d", value, integ.Value)
+
+		return false
+	}
+
+	if integ.TokenLiteral() != fmt.Sprintf("%d", value){
+		t.Errorf("integ.TokenLiteral not %d. got=%s", value, integ.TokenLiteral())
+
+		return false
+	}
+
+	return true
 }
