10462a30d1c272bd7bf5b129e3edd667ca57c544
Mark <the.mark.fullton@gmail.com>
2021-01-09 18:10:21 -0800

    Parser - Added parse Identifier

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"github.com/themarkfullton/go-brainer-interpreter/ast"
 	"github.com/themarkfullton/go-brainer-interpreter/lexer"
 	"github.com/themarkfullton/go-brainer-interpreter/token"
+	"strconv"
 	"fmt"
 	)
 
@@ -193,4 +194,22 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression{
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+
+	if err != nil {
+		msg := fmt.Sprintf("Could not parse %q as integer", p.curToken.Literal)
+
+		p.errors = append(p.errors, msg)
+
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
